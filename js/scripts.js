a9c4316be8e171602510306bc05d543a2219076c
Konaruchan <120046229+Konaruchan@users.noreply.github.com>
2025-02-02 00:12:32 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,22 @@
 async function fetchArticles() {
     try {
         const response = await fetch('articulos/');
+        if (!response.ok) {
+            throw new Error(`Error al cargar la carpeta 'articulos/': ${response.status}`);
+        }
         const data = await response.text();
         const parser = new DOMParser();
         const doc = parser.parseFromString(data, 'text/html');
         const links = doc.querySelectorAll('a');
         const articles = [];
+
         links.forEach(link => {
             const articlePath = link.getAttribute('href');
             if (articlePath.endsWith('/') && articlePath !== '/') {
                 articles.push(articlePath);
             }
         });
+
         const articleData = await Promise.all(articles.map(loadArticleContent));
         return articleData.filter(article => article !== null);
     } catch (error) {
@@ -24,15 +29,21 @@ async function fetchArticles() {
 async function loadArticleContent(articlePath) {
     try {
         const markdownResponse = await fetch(`${articlePath}articulo.md`);
+        if (!markdownResponse.ok) {
+            throw new Error(`Error al cargar el archivo Markdown: ${markdownResponse.status}`);
+        }
         const markdownText = await markdownResponse.text();
+
         // Parsear el contenido Markdown
         const frontmatterRegex = /^---\s*([\s\S]*?)\s*---\s*([\s\S]*)/;
         const match = markdownText.match(frontmatterRegex);
         if (!match) {
             throw new Error('No se encontró el frontmatter en el archivo Markdown.');
         }
+
         const frontmatter = YAML.parse(match[1]);
         const content = marked.parse(match[2]);
+
         // Obtener el nombre de la carpeta como nombre interno del artículo
         const folderName = articlePath.split('/').filter(Boolean)[0];
         return { ...frontmatter, content, path: articlePath, id: folderName };
