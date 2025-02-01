9a3b2400decff8025555b3ef02b2f7022894fc56
Konaruchan <120046229+Konaruchan@users.noreply.github.com>
2025-02-02 00:02:36 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,12 @@ async function fetchArticles() {
         const doc = parser.parseFromString(data, 'text/html');
         const links = doc.querySelectorAll('a');
         const articles = [];
-
         links.forEach(link => {
             const articlePath = link.getAttribute('href');
             if (articlePath.endsWith('/') && articlePath !== '/') {
                 articles.push(articlePath);
             }
         });
-
         const articleData = await Promise.all(articles.map(loadArticleContent));
         return articleData.filter(article => article !== null);
     } catch (error) {
@@ -27,20 +25,16 @@ async function loadArticleContent(articlePath) {
     try {
         const markdownResponse = await fetch(`${articlePath}articulo.md`);
         const markdownText = await markdownResponse.text();
-
         // Parsear el contenido Markdown
         const frontmatterRegex = /^---\s*([\s\S]*?)\s*---\s*([\s\S]*)/;
         const match = markdownText.match(frontmatterRegex);
         if (!match) {
             throw new Error('No se encontró el frontmatter en el archivo Markdown.');
         }
-
         const frontmatter = YAML.parse(match[1]);
         const content = marked.parse(match[2]);
-
         // Obtener el nombre de la carpeta como nombre interno del artículo
         const folderName = articlePath.split('/').filter(Boolean)[0];
-
         return { ...frontmatter, content, path: articlePath, id: folderName };
     } catch (error) {
         console.error(`Error al cargar el artículo ${articlePath}:`, error);
